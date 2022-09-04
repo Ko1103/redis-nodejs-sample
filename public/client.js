aeacd8c98c8728002bbb03272ffcc9060a394b42
山浦功 <ko.foot1103@gmail.com>
2022-09-04 22:15:50 +0900

    :sparkles: add server

--- public/client.js
+++ public/client.js
@@ -1,13 +1,43 @@
-wsButton.onclick = function () {
-    wsSendButton.disabled = false;
 
+(function () {
+  const messages = document.querySelector('#messages');
+  const wsButton = document.querySelector('#wsButton');
+  const wsSendButton = document.querySelector('#wsSendButton');
+
+  function showMessage(message) {
+    messages.textContent += `\n${message}`;
+    messages.scrollTop = messages.scrollHeight;
+  }
+
+  let ws;
+
+  wsButton.onclick = function () {
+    wsSendButton.disabled = false;
+    
     ws = new WebSocket(`ws://${location.host}`);
     ws.onopen = function () {
-        showMessage('WebSocket connection established');
+      showMessage('WebSocket connection established');
     };
     ws.onmessage = function (message) {
-        let data = JSON.parse(message.data)
-        showMessage(JSON.stringify(data.message));
+      let data = JSON.parse(message.data)
+      console.log(data)
+      showMessage(JSON.stringify(data.message));
     };
-     -- 省略 --
-};
+    ws.onclose = function (event) {
+      showMessage('Connection closed.' + event);
+      ws = null;
+    };
+    ws.onerror = function (event) {
+      showMessage('Connection error.' + event);
+    };
+  };
+
+  wsSendButton.onclick = function () {
+    if (!ws) {
+      showMessage('No WebSocket connection');
+      return;
+    }
+    const msg = document.querySelector('#msgInput').value;
+    ws.send(msg);
+  };
+})();
--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const express = require("express");
 const http = require("http");
 const WebSocket = require("ws");
@@ -11,27 +13,52 @@ const wss = new WebSocket.Server({ noServer: true });
 const redis = new Redis("redis"); //redis container
 const client = new Redis("redis");
 
+server.on("upgrade", function (request, socket, head) {
+  wss.handleUpgrade(request, socket, head, function (ws) {
+    wss.emit("connection", ws, request);
+  });
+});
+
 function subscribeMessage(channel) {
   client.subscribe(channel);
-  client.on('message', function(channel, message) {
-    broadcast(JSON.parse(message))
+  client.on("message", function (channel, message) {
+    const buf = Buffer.from(JSON.stringify(message));
+    broadcast(buf.toJSON());
+    // broadcast(JSON.parse(message));
   });
 }
-subscribeMessage('newMessage')
+subscribeMessage("newMessage");
 
 // broadcast message to all clients
-function broadcast(message){
-  wss.clients.forEach(function(client){
-    client.send(JSON.stringify({
-        message: message
-    }))
-  })
+function broadcast(message) {
+  wss.clients.forEach(function (client) {
+    client.send(
+      JSON.stringify({
+        message: message,
+      })
+    );
+  });
 }
 
-wss.on('connection', function (ws, request) {
-  ws.on('message', function (message) {
-    redis.publish('newMessage', JSON.stringify(message))
+wss.on("connection", function (ws, request) {
+  ws.on("message", function (message) {
+    redis.publish("newMessage", JSON.stringify(message));
   });
 
-  -- 省略 --
+  ws.on("close", function (code, reason) {
+    console.log(
+      "Client connection closed. (Code: %s, Reason: %s)",
+      code,
+      reason
+    );
+  });
+
+  ws.on("error", function (error) {
+    console.log("Client connection errored (%s)", error);
+  });
+});
+
+const port = 3000;
+server.listen(port, function () {
+  console.log("Server start...");
 });
